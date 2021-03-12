9a1a60947e883813195942c341277d593044d700
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-12 22:51:07 +1100

    Dom elements added but not passing

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+
+
+window.addEventListener('load', function () {
+    const body = document.body;
+    const main = document.querySelector("main#main");
+    let newHeader = document.createElement('h1');
+    
+    main.remove();
+
+    newHeader.setAttribute('id', 'victory');
+    newHeader.innerText= "NILS is the champion";
+
+    body.appendChild(newHeader);
+
+  });
+
+
+    
